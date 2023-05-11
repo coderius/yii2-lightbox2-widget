ec3f269e6d9536cb0964d483097e8c38d02aff21
Dmitry Crestoff <crestoff@gmail.com>
2023-05-11 14:48:02 +0700

    Update Lightbox2Asset.php

--- src/Lightbox2Asset.php
+++ src/Lightbox2Asset.php
@@ -1,14 +1,6 @@
 <?php
 
-/**
- * @package yii2-extentions
- * @license The MIT License
- * @copyright Copyright (C) 2012-2018 Sergio coderius <coderius>
- * @contacts sunrise4fun@gmail.com - Have suggestions, contact me :) 
- * @link https://github.com/coderius - My github
- */
-
-namespace coderius\lightbox2;
+namespace crestoff\lightbox2;
 
 use Yii;
 use yii\web\AssetBundle;
@@ -17,17 +9,13 @@ class Lightbox2Asset extends AssetBundle
 {
     public $sourcePath = '@bower/lightbox2/dist';
     
-    public $css = [];
-    
     public $js = [];
-    
-//    public $jsOptions = [
-//        'position' => \yii\web\View::POS_END,
-//    ];
+    public $css = [];
     
     public $depends = [
         'yii\web\JqueryAsset',
     ];
+    
     public function init()
     {
         parent::init();
@@ -35,6 +23,4 @@ class Lightbox2Asset extends AssetBundle
         $this->js[] = YII_DEBUG ? 'js/lightbox.js' : 'js/lightbox.min.js';
         $this->css[] = YII_DEBUG ? 'css/lightbox.css' : 'js/lightbox.min.css';
     }
-    
 }
-

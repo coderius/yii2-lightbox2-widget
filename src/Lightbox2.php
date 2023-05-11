afd0e0d1c1acf002d5f15430515894616353b63c
Dmitry Crestoff <crestoff@gmail.com>
2023-05-11 14:48:56 +0700

    Update Lightbox2.php

--- src/Lightbox2.php
+++ src/Lightbox2.php
@@ -1,12 +1,5 @@
 <?php
 
-/**
- * @package yii2-extentions
- * @license The MIT License
- * @copyright Copyright (C) 2012-2018 Sergio coderius <coderius>
- * @contacts sunrise4fun@gmail.com - Have suggestions, contact me :) 
- * @link https://github.com/coderius - My github
- */
 namespace crestoff\lightbox2;
 
 use yii\base\Widget;
@@ -14,35 +7,19 @@ use yii\helpers\Json;
 
 class Lightbox2 extends Widget
 {
-    /**
-     * @var array the options for the lightbox2 JS plugin.
-     * @see https://lokeshdhakar.com/projects/lightbox2/ 
-     */
     public $clientOptions = [];
     
-    /**
-     * @inheritdoc
-     */
     public function init()
     {
-        parent::init();
-        
+        parent::init();       
     } 
     
-    /**
-     * @inheritdoc
-     */
     public function run()
     {
         $plugin = $this->makePlugin('lightbox');
-        $this->registerAssets($plugin);
-        
+        $this->registerAssets($plugin);       
     }
     
-    /**
-     * @param string $name
-     * @return string
-     */
     protected function makePlugin($name)
     {
         $js = false;
@@ -55,17 +32,10 @@ class Lightbox2 extends Widget
             
         return $js;
     }
-    
-    /**
-     * @param string $plugin
-     */
+
     protected function registerAssets($plugin){
         $view = $this->getView();
         $bundle = Lightbox2Asset::register($view);
         $view->registerJs($plugin);
     }
-    
-    
-    
-    
 }
